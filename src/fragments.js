cca29715eea5c3fd0d0dd5646dd6eb8349dc8ac8
Robert <robertmarshallproductions@gmail.com>
2022-04-15 16:04:10 +0100

    Update buttom fragment

--- src/fragments.js
+++ src/fragments.js
@@ -1,40 +1,40 @@
-import { graphql } from 'gatsby'
+import { graphql } from "gatsby";
 
 export const Button = graphql`
-    fragment Button on WpFormButton {
-        conditionalLogic {
-            ...ConditionalLogic
-        }
-        imageUrl
-        text
-        type
+  fragment Button on WpFormSubmitButton {
+    conditionalLogic {
+      ...ConditionalLogic
     }
-`
+    imageUrl
+    text
+    type
+  }
+`;
 
 export const ConditionalLogic = graphql`
-    fragment ConditionalLogic on WpConditionalLogic {
-        actionType
-        logicType
-        rules {
-            fieldId
-            operator
-            value
-        }
+  fragment ConditionalLogic on WpConditionalLogic {
+    actionType
+    logicType
+    rules {
+      fieldId
+      operator
+      value
     }
-`
+  }
+`;
 
 export const FormConfirmation = graphql`
-    fragment FormConfirmation on WpFormConfirmation {
-        conditionalLogic {
-            ...ConditionalLogic
-        }
-        id
-        isDefault
-        message
-        name
-        pageId
-        queryString
-        type
-        url
+  fragment FormConfirmation on WpFormConfirmation {
+    conditionalLogic {
+      ...ConditionalLogic
     }
-`
+    id
+    isDefault
+    message
+    name
+    pageId
+    queryString
+    type
+    url
+  }
+`;
